dffb047a8fcb355f5b57b1ffa665771ba11142eb
Jazmincita13 <125412889+Jazmincita13@users.noreply.github.com>
2023-03-10 19:41:16 -0600

    Add files via upload

---
+++ Actividadsemana7/Actividadsemana7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System; 
+
+Console.WriteLine("▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲");
+Console.WriteLine("FIGURA TRIANGULO RECTANGULO");
+Console.WriteLine("▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲");
+
+
+class For
+{ 
+    
+    static void Main(string[] args)
+    {
+        Console.Write("Favor ingresar un numero entero");
+        int variable = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= variable; i++) 
+        { 
+            for(int j = 1; j <= 1; j++) 
+            { 
+               Console.Write("*");
+            }
+            Console.WriteLine();
+        }       
+    }
+}
